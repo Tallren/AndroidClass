cfa824b471e853ce653a09a1a9febc7f5321bafd
Ian Davis <iandavis17@yahoo.com>
2016-10-03 21:29:33 -0400

    Assigment 5

--- PersonalTrainer/app/src/main/java/com/iandavis/android/personaltrainer/Login.java
+++ PersonalTrainer/app/src/main/java/com/iandavis/android/personaltrainer/Login.java
@@ -4,19 +4,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public Button loginButton;
+    public String correctUsername = getString(R.string.username);
+    public String correctPassword = getString(R.string.password);
+    public EditText usernameInput;
+    public EditText passwordInput;
+    public String username;
+    public String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+        loginButton = (Button) findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
 
-    public void login(View view){
-        Intent intent = new Intent(this, MainMenu.class);
-        startActivity(intent);
+            @Override
+            public void onClick(View v) {
+                usernameInput = (EditText) findViewById(R.id.usernameInput);
+                passwordInput = (EditText) findViewById(R.id.passwordInput);
+                username = usernameInput.getText().toString();
+                password = passwordInput.getText().toString();
+
+                Intent intent = new Intent(Login.this, MainMenu.class);
+
+                if(username.equals(correctUsername) && password.equals(correctPassword)){
+                    startActivity(intent);
+                }
+
+                else{
+                    usernameInput.clearComposingText();
+                    passwordInput.clearComposingText();
+                }
+            }
+        });
     }
 }
